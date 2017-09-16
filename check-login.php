6bff47028177c4335e955751ba31ddeb36eb9112
Hannan Yusop <hannanyusop@lynk.my>
2017-09-16 11:12:29 +0800

    first commit for user layout

--- check-login.php
+++ check-login.php
@@ -1,34 +1,40 @@
 <?php
-require_once 'connection.php';
 
-//get value from login.php
-session_start();
-$email = $_POST['email'];
-$password =$_POST['password'];
+    if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+        header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
+        die( header( '404.php' ) );
 
-//check if credentials in valid and users are not banned
-
-$sql = "SELECT * FROM users WHERE email='".$email."' AND password='".$password."' AND status=0";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-
-    while($row = mysqli_fetch_assoc($result)) {
-
-        $_SESSION['email'] = $row['email'];
-        $_SESSION['level'] = $row['level'];
-        $_SESSION['first_name'] = $row['first_name'];
-        $_SESSION['level'] = $row['level'];
     }
+    require_once 'connection.php';
 
-    if($_SESSION['level'] == 'ADMIN'){
-        echo "<script>window.location='admin/index.php';</script>";
-    }else{
-        echo "<script>alert('User page still on delvelopment : )');window.location='index.php';</script>";
+    //get value from login.php
+    session_start();
+    $email = $_POST['email'];
+    $password =$_POST['password'];
+
+    //check if credentials in valid and users are not banned
+
+    $sql = "SELECT * FROM users WHERE email='".$email."' AND password='".$password."' AND status=0";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+
+        while($row = mysqli_fetch_assoc($result)) {
+
+            $_SESSION['email'] = $row['email'];
+            $_SESSION['level'] = $row['level'];
+            $_SESSION['first_name'] = $row['first_name'];
+            $_SESSION['level'] = $row['level'];
+        }
+
+        if($_SESSION['level'] == 'ADMIN'){
+            echo "<script>window.location='admin/index.php';</script>";
+        }else{
+            echo "<script>window.location='user/index.php';</script>";
+        }
+
+    } else{
+        echo "<script>alert('Invalid Password or Username!');window.location='index.php';</script>";
     }
-
-} else{
-    echo "<script>alert('Invalid Password or Username!');window.location='index.php';</script>";
-}
 
 ?>
--- index.php
+++ index.php
@@ -1,268 +1,187 @@
-<div id="hero">
-    <div id="background_animate"></div>
-    <h1>Internal Tools</h1>
-    <span class="img_holder">
-                <img class="dr-glue-little" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/63425/drglue.png" alt="Dr Glue Little">
-                <div class="left_arm">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/63425/left-arm.png" alt="Left Arm">
-                </div>
-                <div class="right_arm">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/63425/right-arm.png" alt="Right Arm">
-                </div>
-            </span>
+<?php include 'header.php';?>
+
+
+
+
+<!-- banner -->
+<div class="banner">    	   
+    <img src="images/photos/banner.jpg"  class="img-responsive" alt="slide">
+    <div class="welcome-message">
+        <div class="wrap-info">
+            <div class="information">
+                <h1  class="animated fadeInDown">Job Service Hero <br><b>MUKAH</b></h1>
+                <p class="animated fadeInUp">Most luxurious hotel of asia with the royal treatments and excellent customer service.</p>                
+            </div>
+            <a href="#information" class="arrow-nav scroll wowload fadeInDownBig"><i class="fa fa-angle-down"></i></a>
+        </div>
+    </div>
 </div>
-<form action="check-login.php" method="post" accept-charset="utf-8">
-    <div class="input_holder">
-        <span><i class="fa fa-user"></i></span><input type="text" name="email" placeholder="email" required>
+<!-- banner-->
+
+
+<!-- reservation-information -->
+<div id="information" class="spacer reserve-info ">
+<div class="container">
+<div class="row">
+<div class="col-sm-7 col-md-8">
+    <div class="embed-responsive embed-responsive-16by9 wowload fadeInLeft"><iframe  class="embed-responsive-item" src="//player.vimeo.com/video/55057393?title=0" width="100%" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+</div>
+<div class="col-sm-5 col-md-4">
+<h3>Reservation</h3>
+    <form role="form" class="wowload fadeInRight">
+        <div class="form-group">
+            <input type="text" class="form-control"  placeholder="Name">
+        </div>
+        <div class="form-group">
+            <input type="email" class="form-control"  placeholder="Email">
+        </div>
+        <div class="form-group">
+            <input type="Phone" class="form-control"  placeholder="Phone">
+        </div>        
+        <div class="form-group">
+            <div class="row">
+            <div class="col-xs-6">
+            <select class="form-control">
+              <option>No. of Rooms</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+            </div>        
+            <div class="col-xs-6">
+            <select class="form-control">
+              <option>No. of Adult</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+            </div></div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+            <div class="col-xs-4">
+              <select class="form-control col-sm-2" name="expiry-month" id="expiry-month">
+                <option>Date</option>
+                <option value="01">1</option>
+                <option value="02">2</option>
+                <option value="03">Mar (03)</option>
+                <option value="04">Apr (04)</option>
+                <option value="05">May (05)</option>
+                <option value="06">June (06)</option>
+                <option value="07">July (07)</option>
+                <option value="08">Aug (08)</option>
+                <option value="09">Sep (09)</option>
+                <option value="10">Oct (10)</option>
+                <option value="11">Nov (11)</option>
+                <option value="12">Dec (12)</option>
+              </select>
+            </div>
+            <div class="col-xs-4">
+              <select class="form-control col-sm-2" name="expiry-month" id="expiry-month">
+                <option>Month</option>
+                <option value="01">Jan (01)</option>
+                <option value="02">Feb (02)</option>
+                <option value="03">Mar (03)</option>
+                <option value="04">Apr (04)</option>
+                <option value="05">May (05)</option>
+                <option value="06">June (06)</option>
+                <option value="07">July (07)</option>
+                <option value="08">Aug (08)</option>
+                <option value="09">Sep (09)</option>
+                <option value="10">Oct (10)</option>
+                <option value="11">Nov (11)</option>
+                <option value="12">Dec (12)</option>
+              </select>
+            </div>
+            <div class="col-xs-4">
+              <select class="form-control" name="expiry-year">
+                <option value="13">2013</option>
+                <option value="14">2014</option>
+                <option value="15">2015</option>
+                <option value="16">2016</option>
+                <option value="17">2017</option>
+                <option value="18">2018</option>
+                <option value="19">2019</option>
+                <option value="20">2020</option>
+                <option value="21">2021</option>
+                <option value="22">2022</option>
+                <option value="23">2023</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+            <textarea class="form-control"  placeholder="Message" rows="4"></textarea>
+        </div>
+        <button class="btn btn-default">Submit</button>
+    </form>    
+</div>
+</div>  
+</div>
+</div>
+<!-- reservation-information -->
+
+
+
+<!-- services -->
+<div class="spacer services wowload fadeInUp">
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <!-- RoomCarousel -->
+            <div id="RoomCarousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                <div class="item active"><img src="images/photos/8.jpg" class="img-responsive" alt="slide"></div>                
+                <div class="item  height-full"><img src="images/photos/9.jpg"  class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/10.jpg"  class="img-responsive" alt="slide"></div>
+                </div>
+                <!-- Controls -->
+                <a class="left carousel-control" href="#RoomCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+                <a class="right carousel-control" href="#RoomCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
+            </div>
+            <!-- RoomCarousel-->
+            <div class="caption">Rooms<a href="rooms-tariff.php" class="pull-right"><i class="fa fa-edit"></i></a></div>
+        </div>
+
+
+        <div class="col-sm-4">
+            <!-- RoomCarousel -->
+            <div id="TourCarousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                <div class="item active"><img src="images/photos/6.jpg" class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/3.jpg"  class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/4.jpg"  class="img-responsive" alt="slide"></div>
+                </div>
+                <!-- Controls -->
+                <a class="left carousel-control" href="#TourCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+                <a class="right carousel-control" href="#TourCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
+            </div>
+            <!-- RoomCarousel-->
+            <div class="caption">Tour Packages<a href="gallery.php" class="pull-right"><i class="fa fa-edit"></i></a></div>
+        </div>
+
+
+        <div class="col-sm-4">
+            <!-- RoomCarousel -->
+            <div id="FoodCarousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                <div class="item active"><img src="images/photos/1.jpg" class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/2.jpg"  class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/5.jpg"  class="img-responsive" alt="slide"></div>
+                </div>
+                <!-- Controls -->
+                <a class="left carousel-control" href="#FoodCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+                <a class="right carousel-control" href="#FoodCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
+            </div>
+            <!-- RoomCarousel-->
+            <div class="caption">Food and Drinks<a href="gallery.php" class="pull-right"><i class="fa fa-edit"></i></a></div>
+        </div>
     </div>
-    <div class="input_holder">
-        <span><i class="fa fa-lock"></i></span><input type="password" name="password" id="password_field" placeholder="Password" required>
-    </div>
-    <input type="submit" class="submit_button" value="Log in">
-</form>
-<script>
-    window.onload = function() {
-        var p = document.getElementById("password_field");
+</div>
+</div>
+<!-- services -->
 
-        p.onfocus = function() {
-            document.querySelector(".img_holder").className += " password";
-        }
 
-        p.onblur = function() {
-            document.querySelector(".password").className = "img_holder";
-        }
-
-        document.querySelector("body").className = "";
-    }
-</script>
-<style>
-    @import url(https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900);
-    * {
-        margin: 0;
-        padding: 0;
-    }
-    html,
-    body {
-        height: 100%;
-        width: 100%;
-        font-family: "Lato";
-        overflow-x: hidden;
-    }
-    .preload * {
-        -webkit-transition: none !important;
-        -moz-transition: none !important;
-        -ms-transition: none !important;
-        -o-transition: none !important;
-        transition: none !important;
-        -webkit-animation: none !important;
-        -moz-animation: none !important;
-        -ms-animation: none !important;
-        -o-animation: none !important;
-        animation: none !important;
-    }
-    #hero {
-        height: 350px;
-        background: #1CB0EC;
-        position: relative;
-        overflow: hidden;
-    }
-    #hero figure {
-        position: absolute;
-        right: 80px;
-        top: 100px;
-        color: #fff;
-    }
-    #hero figure blockquote {
-        font-weight: 100;
-    }
-    #hero h1 {
-        font-weight: 100;
-        color: #fff;
-        font-size: 88px;
-        padding-top: 60px;
-        padding-left: 80px;
-    }
-    #hero span {
-        position: absolute;
-        bottom: -50px;
-        width: 250px;
-        left: 0;
-        right: 0;
-        margin: 0 auto;
-        z-index: 10;
-    }
-    #hero .img_holder.password .left_arm img {
-        left: 78px;
-        bottom: 47px;
-    }
-    #hero .img_holder.password .right_arm img {
-        right: 86px;
-        bottom: 47px;
-    }
-    #hero .img_holder.password:before {
-        left: 78px;
-        bottom: 105px;
-    }
-    #hero .img_holder.password:after {
-        right: 85px;
-        bottom: 105px;
-    }
-    #hero .img_holder img.dr-glue-little {
-        width: 250px;
-        z-index: 10;
-    }
-    #hero .img_holder:before {
-        content: '';
-        position: absolute;
-        height: 20px;
-        width: 40px;
-        background: #E9C09B;
-        left: 10px;
-        bottom: 40px;
-        z-index: 999;
-        border-radius: 100%;
-        transition: 1s;
-        -webkit-transition: 1s;
-        -moz-transition: 1s;
-    }
-    #hero .img_holder:after {
-        content: '';
-        position: absolute;
-        height: 20px;
-        width: 40px;
-        background: #E9C09B;
-        right: 10px;
-        bottom: 40px;
-        z-index: 999;
-        border-radius: 100%;
-        transition: 1s;
-        -webkit-transition: 1s;
-        -moz-transition: 1s;
-    }
-    #hero .img_holder .left_arm img,
-    #hero .img_holder .right_arm img {
-        position: absolute;
-        transition: 1s;
-        -webkit-transition: 1s;
-        -moz-transition: 1s;
-        z-index: 9999;
-        width: 45px;
-    }
-    #hero .img_holder .left_arm img {
-        left: 9px;
-        bottom: -22px;
-    }
-    #hero .img_holder .right_arm img {
-        right: 7px;
-        bottom: -22px;
-    }
-    form {
-        position: relative;
-        width: 400px;
-        height: 280px;
-        margin: 0 auto;
-        z-index: 999999999;
-    }
-    form .input_holder {
-        position: relative;
-        width: 300px;
-        margin: 0 auto;
-    }
-    form .input_holder:nth-of-type(1) {
-        margin-top: 40px;
-    }
-    form .input_holder:nth-of-type(2) {
-        margin-top: 20px;
-        margin-bottom: 20px;
-    }
-    form .input_holder span {
-        position: absolute;
-        left: 10px;
-        top: 8px;
-    }
-    form .input_holder span:after {
-        content: '';
-        position: absolute;
-        width: 1px;
-        height: 20px;
-        background: #ccc;
-        left: 21px;
-        top: 0;
-    }
-    form .input_holder input {
-        display: block;
-        width: 300px;
-        margin: 0 auto;
-        background: #fafafa;
-        border: 0;
-        outline: 0;
-        margin: 10px 0;
-        padding: 10px;
-        text-indent: 30px;
-        font-weight: 300;
-    }
-    form .input_holder input:-webkit-autofill {
-        -webkit-box-shadow: 0 0 0 1000px #fafafa inset;
-    }
-    form .submit_button {
-        width: 320px;
-        height: 36px;
-        border: 0;
-        color: #fff;
-        position: relative;
-        margin-left: 50px;
-        cursor: pointer;
-        background: #1CB0EC;
-        border-bottom: 4px solid #1091c5;
-    }
-    form .submit_button:active {
-        outline: 0;
-        border-bottom: none;
-    }
-    #footer {
-        color: #000;
-        text-align: center;
-    }
-    #footer p {
-        font-size: 16px;
-    }
-    #footer p a {
-        text-decoration: none;
-        color: #1CB0EC;
-        position: relative;
-        transition: .5s;
-        -webkit-transition: .5s;
-    }
-    #footer p a:hover {
-        color: #4bc0f0;
-    }
-    #footer p a:before,
-    #footer p a:after {
-        content: '';
-        position: absolute;
-        background: #1CB0EC;
-        height: 2px;
-        left: 0;
-        right: 0;
-        opacity: 0;
-        transition: .5s;
-        -webkit-transition: .5s;
-    }
-    #footer p a:before {
-        top: -10px;
-    }
-    #footer p a:after {
-        bottom: -10px;
-    }
-    #footer p a:hover:before,
-    #footer p a:hover:after {
-        opacity: 1;
-    }
-    #footer p a:hover:before {
-        top: -3px;
-    }
-    #footer p a:hover:after {
-        bottom: -5px;
-    }
-
-</style>
+<?php include 'footer.php';?>
