fcf4e3355e2c3be569990e14c1ac970f39b959cc
Hannan Yusop <hannanyusop@lynk.my>
2017-09-16 10:33:51 +0800

    Block direct access for connecttionfile

--- connection.php
+++ connection.php
@@ -1,4 +1,11 @@
 <?php
+//block form direct access for security purpose to prevent from unauthorized person inject sql connection
+if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+    header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
+    die( header( '404.php' ) );
+
+}
+
 $servername = "localhost";
 $username = "root";
 $password = "";
